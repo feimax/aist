9bc0e4f1beca9e23f52f1fbe28e6357e7e7b71d1
feimax <fc@oooole.net>
2019-03-02 23:57:31 +0800

    bug fixed: base class, 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aist",
-    version="0.2.1",
+    version="0.2.2",
     author="Feimax",
     author_email="me@feimax.com",
     description="AIST = AI Service Tools",
