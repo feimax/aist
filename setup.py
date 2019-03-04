67a482cbb33a5462ce2e58df06d384cd2cd3d14f
feimax <fc@oooole.net>
2019-03-04 10:05:59 +0800

    bug fixed: ls empty file name allowed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aist",
-    version="0.2.2",
+    version="0.2.3",
     author="Feimax",
     author_email="me@feimax.com",
     description="AIST = AI Service Tools",
